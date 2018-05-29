f677a878aa448cba24f02a7dded378f64f2b7043
Ben Leggiero <BenLeggiero@Gmail.com>
2018-05-29 17:48:53 -0400

    Initial Commit

---
+++ Dark Mode Switcher/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Dark Mode Switcher
+//
+//  Created by Ben Leggiero on 2018-05-29.
+//  Copyright © 2018 Ben Leggiero. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Dark Mode Switcher/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Dark Mode Switcher
+//
+//  Created by Ben Leggiero on 2018-05-29.
+//  Copyright © 2018 Ben Leggiero. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
